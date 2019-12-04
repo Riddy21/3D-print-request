310efc84f279a5cd3a28d0e6d1b2947c91e0d7e5
Riddy21 <songt7@mcmaster.ca>
2019-12-04 13:20:14 -0500

    Changed self.StartMenu to self.backToMenu for some windows

--- GUI.py
+++ GUI.py
@@ -315,6 +315,7 @@ class Window:
         tk.Button(self.infoFrame, text=text, command=function).pack()
         tk.Button(self.infoFrame, text="Back to Menu", command=self.destroyFrame).pack()
 
+
     def destroyFrame(self):
         self.infoFrame.destroy()
         self.backToMenu()
